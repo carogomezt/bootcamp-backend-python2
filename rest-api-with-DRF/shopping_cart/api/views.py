f1fd09ca2d0886c62c571be5d6cf2bdcac8ba965
Carolina Gómez Trejos <carolina9511@gmail.com>
2023-09-04 21:29:55 -0500

    feat: rename file

--- rest-api-with-DRF/shopping_cart/api/views.py
+++ rest-api-with-DRF/shopping_cart/api/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import CartItem
-from .serializer import CartItemSerializer
+from .serializers import CartItemSerializer
 
 
 # Create your views here.
