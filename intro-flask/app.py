c6afa31bd64722522b6002378dced5afb90b8f96
Carolina Gómez Trejos <carolina9511@gmail.com>
2025-03-03 21:29:39 -0500

    feat: update flask example

--- intro-flask/app.py
+++ intro-flask/app.py
@@ -53,7 +53,7 @@ def login():
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out.')
-    return redirect(url_for('welcome'))
+    return redirect(url_for('home'))
 
 
 # start the server with the 'run()' method
